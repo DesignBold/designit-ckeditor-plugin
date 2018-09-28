ef709bea3f5015b9325f1354f7d5e1bc6519ffac
HiepTV <35910113+ffse1701003@users.noreply.github.com>
2018-09-28 16:55:02 +0700

    test 28/09/2018

---
+++ app.js
@@ -0,0 +1,22 @@
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Image from '@ckeditor/ckeditor5-image/src/image';
+import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
+
+import InsertImage from './myPlugin';
+
+ClassicEditor
+    .create( document.querySelector( '#editor' ), {
+        plugins: [ Essentials, Paragraph, Bold, Italic, Image, InsertImage, ImageCaption ],
+        toolbar: [ 'bold', 'italic', 'insertImage' ]
+    } )
+    .then( editor => {
+        console.log( 'Editor was initialized', editor );
+    } )
+    .catch( error => {
+        console.error( error.stack );
+    } );
